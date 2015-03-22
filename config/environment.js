20d5713e96f12c5aaaf367d07c7cef7712d01d22
joesiewert <joe.siewert@gmail.com>
2015-03-21 19:58:02 -0600

    Configure Devise

--- config/environment.js
+++ config/environment.js
@@ -19,6 +19,14 @@ module.exports = function(environment) {
     }
   };
 
+  ENV['simple-auth'] = {
+    authorizer: 'simple-auth-authorizer:devise'
+  };
+
+  ENV['simple-auth-devise'] = {
+    identificationAttributeName: 'email'
+  };
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
